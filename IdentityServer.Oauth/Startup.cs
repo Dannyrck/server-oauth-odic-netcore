b39792cba618c4f669296f50106fedc693f95c3b
Oscar Daniel Perez Espinoza <odprz.dev@gmail.com>
2020-04-02 10:09:53 -0600

    feat: se agrega soporte para UI en el servidor

--- IdentityServer.Oauth/Startup.cs
+++ IdentityServer.Oauth/Startup.cs
@@ -8,6 +8,7 @@ using IdentityServer.Oauth.InMemoryConfigurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -16,24 +17,35 @@ namespace IdentityServer.Oauth
 {
     public class Startup
     {
+        public IWebHostEnvironment Environment { get; set; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            Environment = env;
+        }
+
+       
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //var signinCertificate = Path.Combine(_env.ContentRootPath, String.Format("Certificates/test.pfx")); //variable que contiene el certificado para las firmas
+
+            var signinCertificate = Path.Combine(Environment.ContentRootPath, String.Format("Certificates/test.pfx")); //variable que contiene el certificado para las firmas
             var passwordSignin = "test"; // password con el que se protegio el certificado.pfx
+
             services.AddIdentityServer()
-                .AddSigningCredential(new X509Certificate2(@"C:\Users\Danny\source\otherRepos\netcoreIdentitySamples\IdentityPlueralSight\IdentityServer.Oauth\Certificates\test.pfx", passwordSignin)) // utilizar para produccion Certificado x509 para firma
+                .AddSigningCredential(new X509Certificate2(signinCertificate, passwordSignin)) // utilizar para produccion Certificado x509 para firma
                 //.AddDeveloperSigningCredential() // solo para desarrollo
                 .AddTestUsers(InMemoryConfiguration.Users().ToList())
                 .AddInMemoryClients(InMemoryConfiguration.Clents())
                 .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+
+            // agregando soporte para mvc
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env 
-            //, ILoggingBuilder loggerFactory
-            )
+        public void Configure(IApplicationBuilder app )
         {
             //if (env.IsDevelopment())
             //{
@@ -53,6 +65,15 @@ namespace IdentityServer.Oauth
             app.UseDeveloperExceptionPage();
 
             app.UseIdentityServer();
+
+            // Agregando soporte para MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
