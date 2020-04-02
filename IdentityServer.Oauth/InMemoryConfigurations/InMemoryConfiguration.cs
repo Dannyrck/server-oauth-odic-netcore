93f9e6a5318acf04b218fa0b2c5a72ebf34ce6b6
Oscar Daniel Perez Espinoza <odprz.dev@gmail.com>
2020-04-01 22:32:26 -0600

    feat: configuracion inicial de un servidor de autorizacion basado en oauth y oidc

---
+++ IdentityServer.Oauth/InMemoryConfigurations/InMemoryConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Oauth.InMemoryConfigurations
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("myapi","Ejemplo de cliente")
+            };
+        }
+
+        public static IEnumerable<Client> Clents()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId="clientearestapi",
+                    ClientSecrets= new [] {new Secret("clavesecreta".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new []{"myapi"}
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "daniel",
+                    Password = "Qwertyu123!",
+                },
+                 new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "oscar",
+                    Password = "Qwertyu123!",
+                }
+            };
+        }
+    }
+}
